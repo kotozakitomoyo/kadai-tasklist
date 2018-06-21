e5aba0734a32381ae9bf438cc73db873d5669163
tomoyo kotozaki <kotozaki.tomoyo@videomarket.co.jp>
2018-06-21 17:12:05 +0900

    dto dao persistence

---
+++ src/models/Tasklist.java
@@ -0,0 +1,77 @@
+package models;
+
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+
+@Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTasks",
+            query = "select t from Tasklist as t order by t.id desc"
+            )
+})
+@Table(name = "tasks")
+public class Tasklist {
+
+    //カラム
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "content", length = 255, nullable = false)
+    private String content;
+
+    @Column(name = "start_date", nullable = false)
+    private Date start_date;
+
+    @Column(name = "deadline")
+    private Date deadline;
+
+
+
+    //ゲッターセッター
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+}
